244026cf5b728a61f266a451bd1e01b8ccbda569
Solarin <solarinjohnson@gmail.com>
2023-08-20 13:13:31 +0100

    API

---
+++ news.js
@@ -0,0 +1,9 @@
+var url = 'https://newsapi.org/v2/everything?q=sugar-crush&apiKey=ca149d94ec5f47d48a80f0844ca39eba'
+
+var req = new Request(url);
+
+fetch(req)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.articles[0].title)
+    })
