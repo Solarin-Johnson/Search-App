80b525588538dc82309641a76ecb955309051c8a
Solarin <solarinjohnson@gmail.com>
2023-08-20 23:52:00 +0100

    changes

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 autosize = setInterval(() => {
     if (innerWidth < 500) {
         document.getElementById("container").style.width = `${innerWidth - 0.2}px`
+    } else {
+        document.getElementById("container").style.width = "500px"
     }
-}, 10);
+}, 1);
+var service = "news"
+
+document.getElementById("news").addEventListener("click", () => {
+    alert("hhdh")
+})
